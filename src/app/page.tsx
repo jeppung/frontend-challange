78c32b613e243ea7066f0f926e8bdfa6ca60366a
jeppung <jeppung@gmail.com>
2024-03-29 15:51:16 +0700

    feat: Adding initial header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="shadow p-4">
+        <div className="container mx-auto flex justify-between">
+          <h1>Frontend CRUD</h1>
+          <nav>
+            <ul className="flex gap-x-5">
+              <Link href={""}>Blogs</Link>
+              <Link href={""}>Users</Link>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
 }
