98b66b1a2992f2e10298a1b7d560b6ba4e57956b
jeppung <jeppung@gmail.com>
2024-03-29 20:43:24 +0700

    feat: Adding suspense boundary

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
 import Header from "../components/Header";
+import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Header />
-        {children}
+        <Suspense>{children}</Suspense>
       </body>
     </html>
   );
