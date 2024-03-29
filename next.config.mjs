890c3bb380b3c83bc9195022fd979b72bdcdd1e2
jeppung <jeppung@gmail.com>
2024-03-29 23:25:26 +0700

    feat: Adding api token environment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        API_TOKEN: '7e9a8e2163204c539d8633d0f9977d5664c00cc48c134a4b6789d47626100f9d'
+    }
+};
 
 export default nextConfig;
