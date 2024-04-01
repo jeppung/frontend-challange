f3b33d4297eb3ea1814c0d7f34f254d38e46456c
jeppung <jeppung@gmail.com>
2024-04-01 10:34:39 +0700

    feat: Adding user dialog component

---
+++ src/components/UserDialog.tsx
@@ -0,0 +1,131 @@
+import { useState } from "react";
+import { IUser } from "../app/blogs/[id]/page";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import { Label } from "./ui/label";
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectItem,
+} from "./ui/select";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+
+export interface ICreateUserRequest {
+  name: string;
+  gender: string;
+  email: string;
+  status: string;
+}
+
+interface ICreateUserProps {
+  onSubmit: (data: ICreateUserRequest) => void;
+  useButton?: boolean;
+  Icon?: JSX.Element;
+  title: string;
+  userData?: IUser;
+}
+
+export function UserDialog({
+  onSubmit,
+  userData,
+  useButton,
+  Icon,
+  title,
+}: ICreateUserProps) {
+  const [data, setData] = useState<ICreateUserRequest>(
+    userData
+      ? userData
+      : {
+          name: "",
+          email: "",
+          gender: "",
+          status: "",
+        }
+  );
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  return (
+    <Dialog open={isOpen} onOpenChange={(v) => setIsOpen(v)}>
+      <DialogTrigger asChild>
+        {useButton ? (
+          <Button onClick={() => setIsOpen(!isOpen)}>{title}</Button>
+        ) : (
+          Icon
+        )}
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+        </DialogHeader>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            setIsOpen(false);
+            onSubmit(data);
+          }}
+          className="flex flex-col gap-2"
+        >
+          <div>
+            <Label>Name</Label>
+            <Input
+              type="text"
+              onChange={(e) => setData({ ...data, name: e.target.value })}
+              required
+            />
+          </div>
+          <div>
+            <Label>Gender</Label>
+            <Select
+              onValueChange={(v) => setData({ ...data, gender: v })}
+              required
+            >
+              <SelectTrigger>
+                <SelectValue />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="male">Male</SelectItem>
+                <SelectItem value="female">Female</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+          <div>
+            <Label>Email</Label>
+            <Input
+              type="email"
+              onChange={(e) => setData({ ...data, email: e.target.value })}
+              required
+            />
+          </div>
+          <div>
+            <Label>Status</Label>
+            <Select
+              onValueChange={(v) => setData({ ...data, status: v })}
+              required
+            >
+              <SelectTrigger>
+                <SelectValue />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="active">Active</SelectItem>
+                <SelectItem value="inactive">Inactive</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+          <DialogFooter>
+            <Button type="submit">Create</Button>
+          </DialogFooter>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+}
