ef196a4f13844068ee8a1e05869c1fa23b6f006e
jeppung <jeppung@gmail.com>
2024-04-01 17:54:33 +0700

    feat: Adding not-found handler page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="text-center py-10">
+      <h1 className="text-3xl font-bold">Site Not Found</h1>
+      <p>Well, this is awkward. The site you are looking for is not here</p>
+    </div>
+  );
+}
